da3757d977d6bfbad10927dbb9f1805739cdfedd
ysparrk <youngseo0703@gmail.com>
2024-01-04 01:13:43 +0900

    feat: Hello World 및 주석

---
+++ java-start/src/CommentJava.java
@@ -0,0 +1,13 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("hello java1");  // 한 줄 주석
+//        System.out.println("hello java2");  -> 주석으로 무시
+
+        /*
+        여러 줄 주석
+        System.out.println("hello java3");
+        System.out.println("hello java4");
+         */
+
+    }
+}
---
+++ java-start/src/HelloJava.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+
+    // java는 main(String[] args) 메서드를 찾아서 프로그램을 시작한다.
+    public static void main(String[] args) {
+        System.out.println("hello java");   // 문자열 사용 ""
+    }
+}
