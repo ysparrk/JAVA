dc5a2237c116b37a0a2f74ec42f2fe079ec20c8e
ysparrk <youngseo0703@gmail.com>
2024-01-05 00:35:25 +0900

    feat: 변수 및 변수 초기화

---
+++ java-start/src/variable/Var4.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Var4 {
+    // 변수 선언
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        int c, d;  // 여러 개 선언 가능
+    }
+}
---
+++ java-start/src/variable/Var6.java
@@ -0,0 +1,8 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a;
+//        System.out.println(a);  // 컴파일 에러 발생 -> 초기화되지 않았다
+    }
+}
