cb02bcc4e0305b2cd3770749638d2dab6d4040ab
ysparrk <youngseo0703@gmail.com>
2024-01-08 12:35:56 +0900

    feat: 변수 예시 문제 풀이

---
+++ java-start/src/variable/ex/VarEx3.java
@@ -0,0 +1,15 @@
+package variable.ex;
+
+public class VarEx3 {
+    public static void main(String[] args) {
+        /*
+        long 타입의 변수를 선언하고, 그 변수를 10000000000(백억)으로 초기화한 후 출력하는 프로그램을 작성하세요.
+        boolean` 타입의 변수를 선언하고, 그 변수를 `true` 로 초기화한 후 출력하는 프로그램을 작성하세요.
+         */
+        long longVar = 10000000000L;
+        System.out.println(longVar);
+
+        boolean booleanVar = true;
+        System.out.println(booleanVar);
+    }
+}
