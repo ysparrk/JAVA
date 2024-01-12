7c8711a358ce92e7dc950cc1ce510964c0a4dd6b
ysparrk <youngseo0703@gmail.com>
2024-01-12 19:50:11 +0900

    feat: 반복문

---
+++ java-start/src/loop/ex/ForEx2.java
@@ -0,0 +1,11 @@
+package loop.ex;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+        int num = 2;
+        for (int cnt = 0; cnt < 10; cnt++) {
+            System.out.println(num);
+            num += 2;
+        }
+    }
+}
